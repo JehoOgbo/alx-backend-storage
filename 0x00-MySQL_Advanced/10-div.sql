64b642e0dcbbccdd33d4a59a5c4122d15d06d88a
Urom Jehoshaphat Ogbonnia <jehoshaphaturom@gmail.com>
2025-11-06 22:15:40 +0100

    learn about view and functions in sql

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,12 +1,17 @@
 -- function that divides the first number by the second
 -- return divident or 0 if second number is 0
+DROP FUNCTION IF EXISTS SafeDiv;
 DELIMITER $$
 CREATE FUNCTION SafeDiv(a INT, b INT)
 RETURNS FLOAT
 DETERMINISTIC
 BEGIN
 	DECLARE DIVIDEND FLOAT;
-	SET DIVIDEND = a / b;
+	IF b = 0 THEN
+		SET DIVIDEND = 0;
+	ELSE
+		SET DIVIDEND = a / b;
+	END IF;
 
 	RETURN DIVIDEND;
 END $$
